cb711ab1366f1d49ddd2dd3e92c2ecfa07dff6a3
Miller <millerk9@wit.edu>
2021-09-29 06:06:29 -0400

    Lesson 8 of flutter tutorial, adding images to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Scaffold(
-  appBar: AppBar(
-    title: Text('My App'),
-    centerTitle: true,
-    backgroundColor: Colors.red[600],
-  ),
-  body: Center(
-    child: Text('Hello World',
-      style: TextStyle(
-        fontSize: 20.0,
-        fontWeight:FontWeight.bold,
-       letterSpacing: 2.0,
-       color: Colors.grey[600],
-        fontFamily: 'IndieFlower',
-      ),
-    ),
-  ),
-  floatingActionButton: FloatingActionButton(
-  child: Text('Click'),
-    onPressed: () {},
-    backgroundColor: Colors.red[600],
-  ),
-),
+void main() => runApp(MaterialApp(home: Home(),
 ));
 
+
+class Home extends StatelessWidget {
+  @override
+  Widget  build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset('assets/space-3.jpg'),
+        /*child: Text('Hello World',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight:FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),*/
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
 
